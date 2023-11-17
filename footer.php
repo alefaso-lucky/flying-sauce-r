1879ddb121255d2d6eb069f4b3825cd4a409a900
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2023-11-17 16:28:29 +0100

    Il footer era basato sui float ora è basato sui flexbox

--- footer.php
+++ footer.php
@@ -1,37 +1,30 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="./footer_style.css" type="text/css">
-    </head>
-    <body>
-        <div id="footer_container">
-            <div id="upper_footer_container">
-                <div id="newsletter_text">
-                    <h1>Iscriviti alla nostra newsletter!</h1>
-                    <p>Riceverai sconti, regali e inviti ad eventi!</p>
-                </div>
-                <form action="#UNKNOWN" method="post" id="newsletter_form">
-                    <label for="newsletter_field">e-mail: <input type="email" id="newsletter_field" name="email_newsletter" required></label>
-                    <input type="sumbit" value="REGISTRAMI">
-                </form>
-            </div>
-            <div id="lower_footer_container">
-                <div id="trademark_footer">
-                    <p>FLYING SAUCE&reg;</p>
-                    <p>2023-2024</p>
-                </div>
-                <div id="policies_footer">
-                    <p>LINK UTILI</p>
-                    <p>Privacy Policy</p>
-                    <p>Cookie Policy</p>
-                </div>
-                <div id="social_footer">
-                    <p>SEGUICI SU</p>
-                    <a href="#"><img src="./media/facebook_icon.png" alt="facebook icon" width="25px" height="25px"></a>
-                    <a href="#"><img src="./media/instagram_icon.png" alt="instagram icon" width="25px" height="25px"></a>
-                    <a href="#"><img src="./media/youtube_icon.png" alt="youtube icon" width="25px" height="25px"></a>
-                </div>
-            </div>
+<link rel="stylesheet" href="./footer_style.css" type="text/css">
+<div id="footer_container">
+    <div id="upper_footer_container">
+        <div id="newsletter_text">
+            <h1>Iscriviti alla nostra newsletter!</h1>
+            <p>Riceverai sconti, regali e inviti ad eventi!</p>
         </div>
-    </body>
-</html>
+        <form action="#UNKNOWN" method="post" id="newsletter_form">
+            <label for="newsletter_field">e-mail: <input type="email" id="newsletter_field" name="email_newsletter" required></label>
+            <input type="sumbit" value="REGISTRAMI">
+        </form>
+    </div>
+    <div id="lower_footer_container">
+        <div id="trademark_footer">
+            <p>FLYING SAUCE&reg;</p>
+            <p>2023-2024</p>
+        </div>
+        <div id="policies_footer">
+            <p>LINK UTILI</p>
+            <p>Privacy Policy</p>
+            <p>Cookie Policy</p>
+        </div>
+        <div id="social_footer">
+            <p>SEGUICI SU</p>
+            <a href="#"><img src="./media/facebook_icon.png" alt="facebook icon" width="25px" height="25px"></a>
+            <a href="#"><img src="./media/instagram_icon.png" alt="instagram icon" width="25px" height="25px"></a>
+            <a href="#"><img src="./media/youtube_icon.png" alt="youtube icon" width="25px" height="25px"></a>
+        </div>
+    </div>
+</div>
---
+++ mainpage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Flying Sauce&reg;</title>
+    </head>
+    <body>
+        <?php
+            include "footer.php";
+        ?>
+    </body>
+</html>
