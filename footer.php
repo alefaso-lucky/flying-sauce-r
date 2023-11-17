30f0193ecd284aa16b71cf01f3264ec0b4574d58
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2023-11-17 12:32:59 +0100

    Footer grezzo

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./footer_style.css" type="text/css">
+    </head>
+    <body>
+        <div id="footer_container">
+            <div id="upper_footer_container">
+                <div id="newsletter_text">
+                    <h1>Iscriviti alla nostra newsletter!</h1>
+                    <p>Riceverai sconti, regali e inviti ad eventi!</p>
+                </div>
+                <form action="#UNKNOWN" method="post" id="newsletter_form">
+                    <label for="newsletter_field">e-mail: <input type="email" id="newsletter_field" name="email_newsletter" required></label>
+                    <input type="sumbit" value="REGISTRAMI">
+                </form>
+            </div>
+            <div id="lower_footer_container">
+                <div id="trademark_footer">
+                    <p>FLYING SAUCE&reg;</p>
+                    <p>2023-2024</p>
+                </div>
+                <div id="policies_footer">
+                    <p>LINK UTILI</p>
+                    <p>Privacy Policy</p>
+                    <p>Cookie Policy</p>
+                </div>
+                <div id="social_footer">
+                    <p>SEGUICI SU</p>
+                    <img src="./media/facebook_icon.png" alt="facebook icon" width="25px" height="25px">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
