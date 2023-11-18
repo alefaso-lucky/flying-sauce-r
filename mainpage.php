025e82644a5889d838dfaf729526c7384f2ca4df
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2023-11-18 12:23:01 +0100

    aggiunta commenti al footer_style

--- mainpage.php
+++ mainpage.php
@@ -2,8 +2,10 @@
 <html>
     <head>
         <title>Flying Sauce&reg;</title>
+        <meta charset="utf-8"/>
     </head>
     <body>
+        
         <?php
             include "footer.php";
         ?>
