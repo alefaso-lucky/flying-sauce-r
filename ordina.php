80bcdc3d29e193c678cf1c2b6dd7103de9f4e45b
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2023-11-26 10:01:38 +0100

    pagina ordina

---
+++ ordina.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <title>ordina ora</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="ordina.css">
+    </head>
+    <body>
+        
+        <div class="row">
+        <div class="column sx" >Column</div>
+        <div class="column middle" >Column</div>
+        <div class="column dx" >Column</div>
+        </div>
+
+    </body>
+</HTML>
