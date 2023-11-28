439218bfc81032bf75a468cf1e21708a32776369
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2023-11-28 11:10:41 +0100

    stile creaAccount e creazione pag condizioni

---
+++ crea account/informative/condizioni.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" href="infoPrivacy.css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	</head>
+
+	<body>
+		<?php //require('nav.html'); ?> 
+		<div class="cell">
+            <h1>Condizioni</h1>
+            <h2>Condizioni di Iscrizione a Flying Sauce</h2>
+            <p>
+                Benvenuto su Flying Sauce, il ristorante virtuale che ti porta il gusto autentico della pasta italiana direttamente a casa tua tramite droni! Prima di iniziare il tuo viaggio culinario con noi, ti preghiamo di leggere attentamente le seguenti condizioni di iscrizione:
+            </p>
+            <ol>
+                <li> 
+                    <h3>Accettazione delle Condizioni:</h3>
+                    <p>
+                        Con l'iscrizione a Flying Sauce, accetti di essere vincolato da tutte le condizioni e le disposizioni indicate in questo documento. Ti preghiamo di non utilizzare i nostri servizi se non accetti integralmente queste condizioni.
+                    </p>
+                </li>
+                <li> 
+                    <h3>Età Minima:</h3>
+                    <p>
+                        Per iscriverti a Flying Sauce, devi avere almeno 18 anni o l'età legalmente richiesta nel tuo paese di residenza per stipulare contratti. L'utente è responsabile di garantire che tutte le informazioni fornite durante il processo di iscrizione siano accurate e complete.
+                    </p>
+                </li>
+                <li> 
+                    <h3>Responsabilità dell'Utente:</h3>
+                    <p>
+                        Selezionando il servizio di consegna tramite droni offerto da Flying Sauce, riconosci e accetti che l'utilizzo dei droni è soggetto alle leggi locali e alle regolamentazioni vigenti nel tuo paese di residenza. L'utente è responsabile di rispettare tali leggi e regolamentazioni.
+                    </p>
+                </li>
+                <li> 
+                    <h3>Qualità e Origine del Prodotto:</h3>
+                    <p>
+                        Flying Sauce si impegna a offrire prodotti di alta qualità, mantenendo gli standard della cucina italiana e garantendo la consegna tramite droni in tutto il mondo. La nostra pasta è preparata con ingredienti autentici e seguendo ricette tradizionali. Facciamo inoltre presente che la certificazione Michelin menzionata si riferisce alle stelle Michelin assegnate ai ristoranti tradizionali e non a quelli operanti tramite droni.
+                    </p>
+                </li>
+                <li> 
+                    <h3>Pagamenti e Trattamento dei Dati:</h3>
+                    <p>I pagamenti su Flying Sauce sono gestiti attraverso modalità sicure e conformi agli standard di sicurezza del settore. Tutte le informazioni finanziarie sono trattate in conformità con la nostra Informativa sulla Privacy. Ti invitiamo a leggere attentamente tale informativa per comprendere come trattiamo i tuoi dati personali.</p>
+                </li>
+            </ol>
+            <p>Flying Sauce si riserva il diritto di modificare queste condizioni in qualsiasi momento. Le modifiche saranno comunicate agli utenti tramite il sito web o altre forme di comunicazione. L'uso continuato dei nostri servizi dopo tali modifiche costituirà accettazione delle stesse.
+                Grazie per aver scelto Flying Sauce per soddisfare la tua passione per la pasta italiana. Siamo entusiasti di portare un pezzo d'Italia direttamente a casa tua!
+            </p>
+	    </div>
+	</body>
+</html>
