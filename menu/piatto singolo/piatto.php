6a9a4836135cd85364d95dd66829dd30092230e3
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2023-12-05 12:29:37 +0100

    piatto singolo è stato aggiornato

--- menu/piatto singolo/piatto.php
+++ menu/piatto singolo/piatto.php
@@ -3,7 +3,7 @@
     <head>
         <title>piatto selezionato</title>
         <meta character="utf-8">
-        <link rel="stylesheet" href="menusimple.css">
+        <link rel="stylesheet" href="./piatto.css">
     </head>
     <body>
     <?php
@@ -16,21 +16,33 @@
     ?>
     <!-- portare in qualche modo il piatto selezionato in $curr_nom -->
         <?php
-            $sql = "SELECT nome, lista_ingredienti, descrizione_lunga, prezzo, foto,  FROM menu WHERE nome = '$curr_nom'";
+            $sql = "SELECT nome, lista_ingredienti, descrizione_lunga, prezzo, foto  FROM menu WHERE nome = 'Spaghetti alla Carbonara'";
             $ret = pg_query($db, $sql);
+            $row = pg_fetch_array($ret);    
+            $nome = $row[0];
+            $lista_ingredienti = $row[1];
+            $descrizione_lunga = $row[2];
+            $prezzo = $row[3];
+            $foto = $row[4];
         ?>
         <div id="rowUno">
-            <h1><?php echo $nome ?></h1>
-            <?php echo $lista_ingredienti ?>
-            <img src="<?php echo $foto ?>" alt="piatto di pasta" width="400px">
-            <a href="#">TORNA AL MENU</a>
+            <div id="rowUno_leftside">
+                <h1><?php echo $nome ?></h1>
+                <p><?php echo $lista_ingredienti ?></p>
+                <a href="#">ORDINA ORA</a>
+                <a href="#">TORNA AL MENU</a>    
+            </div>
+            <img id="rowUno_rightside" src="../<?php echo $foto ?>" alt="piatto di pasta" width="400px">
         </div>
-        <div id="rowDue">
+        <!-- 
+            <div id="rowDue">
             <div id="sinGlu">
-                <img src="../media/sinGlu.jpg" alt="gluten free" width="30px">
+                <img src="../../media/sinGlu.jpg" alt="gluten free" width="30px">
                 <p>Questo prodotto disponibile anche in versione gluten free</p>
             </div>
-            <?php echo $descrizione_lunga ?>
-        </div>
+            <?php //echo $descrizione_lunga ?>
+            </div>
+         -->
+        
     </body>
 </html>
