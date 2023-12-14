e66454a993c4f16f8182a2a1025cfb965b6032e2
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2023-12-14 18:09:15 +0100

    piccole modifiche a creaAccount

--- crea account/creaAccount.php
+++ crea account/creaAccount.php
@@ -9,48 +9,37 @@
 	<body>
 		<form action="#" method="post" id="main-form">
 				<h1>CREA UN NUOVO ACCOUNT</h1>
-				<p>
-					<input class="name-field insert" name="name" placeholder="Nome" required/>
-					<input class="name-field insert" name="surname" placeholder="Cognome"/>
-				</p>
-                <p>
-                    <input id="e-mail-field insert" name="e-mail" type="email" placeholder="e-Mail" required/>
-                </p>
-				<p>
+				<fieldset>
+                	<legend>Informazioni di Accesso</legend>
+					<input class="e-mail-field insert" name="e-mail" type="email" placeholder="e-Mail" required/>
 					<input class="password-field insert" name="Password" type="password" placeholder="Password"/>
-                    <input class="password-field insert" name="Password" type="password" placeholder="Conferma password"/>
-				</p>
-                <fieldset>
-                <legend>Dati personali</legend>
-					<p>
-						Genere:
-						<label for="male-field">
-							<input id="male-field" name="sesso" type="radio" checked/>Uomo
-						</label>
-						<label for="female-field">
-							<input id="female-field" name="sesso" type="radio"/>Donna
-						</label>
-						<label for="noGender-field">
-							<input id="noGender-field" name="sesso" type="radio"/>Non specificato
-						</label>
-					</p>
-					<p>
-						<label for="data-field">
-							Data di nascita:<input id="data-field" name="data" type="date">
-						</label>
-					</p>
+					<input class="password-field insert" name="Password" type="password" placeholder="Conferma password"/>
 				</fieldset>
 				<fieldset>
-				<legend>Indirizzo</legend>
-					<p>
-						<input class="indirizzo-field insert" name="nation" placeholder="Nazione"/>
-						<input class="indirizzo-field insert" name="region" placeholder="Regione"/>
-						<input class="indirizzo-field insert" name="city" placeholder="Città"/>
-					</p>
-					<p>
-						<input class="indirizzoSpec-field insert" name="street" placeholder="Via o piazza"/>
-						<input class="indirizzoSpec-field insert" name="number" placeholder="Numero civico"/>
-					</p>
+                	<legend>Dati Personali</legend>
+					<input class="name-field insert" name="name" placeholder="Nome" required/>
+					<input class="name-field insert" name="surname" placeholder="Cognome"/>
+					Genere:
+					<label for="male-field">
+						<input id="male-field" name="sesso" type="radio" checked/>Uomo
+					</label>
+					<label for="female-field">
+						<input id="female-field" name="sesso" type="radio"/>Donna
+					</label>
+					<label for="noGender-field">
+						<input id="noGender-field" name="sesso" type="radio"/>Non specificato
+					</label>
+					<label for="data-field">
+						Data di nascita:<input id="data-field" name="data" type="date">
+					</label>
+				</fieldset>
+				<fieldset>
+					<legend>Indirizzo</legend>
+					<input class="indirizzo-field insert" name="nation" placeholder="Nazione"/>
+					<input class="indirizzo-field insert" name="region" placeholder="Regione"/>
+					<input class="indirizzo-field insert" name="city" placeholder="Città"/>
+					<input class="indirizzoSpec-field insert" name="street" placeholder="Via o piazza"/>
+					<input class="indirizzoSpec-field insert" name="number" placeholder="Numero civico"/>
 				</fieldset>
 			<p id="informativa">
             Cliccando su Iscriviti, accetti le nostre <a href="./informative/condizioni.php">Condizioni</a>. Scopri in che modo
