8291fdc5ae8d2c90eb621ff7e4d8ba5cbbd5c10c
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2023-12-23 13:28:02 +0100

    lavorazione di componi piatto

--- componi piatto/cPiattoDef.js
+++ componi piatto/cPiattoDef.js
@@ -10,6 +10,6 @@ function printQuantita(name) {
     var myImg = new Image(597, 618);
     myImg.src = "media/" + name;
     myImg.onload = function() {
-        c.getContext('2d').drawImage(myImg, 205, 460);
+        c.getContext('2d').drawImage(myImg, 50, 442);
     }
 }
