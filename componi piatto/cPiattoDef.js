d502e553790559b1e7612ee3b77ab645b68a76a9
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-01-05 17:55:34 +0100

    Altri commenti a Componi Piatto

--- componi piatto/cPiattoDef.js
+++ componi piatto/cPiattoDef.js
@@ -1,15 +1,23 @@
-var c = document.getElementById("piattoComposto");
-var myImg = new Image(597, 618);
-myImg.src = "media/sfondoComponi.PNG";
-myImg.onload = function() {
-    c.getContext('2d').drawImage(myImg, 0,0);
+var c = document.getElementById("piattoComposto"); /* prende la reference al Canvas per mezzo dell'ID */
+var myImg = new Image(597, 618); /* crea un nuovo oggetto immagine di dimensioni pari al Canvas */
+myImg.src = "media/sfondoComponi.PNG"; /* imposta la src dell'immagine */
+myImg.onload = function() { /* si assicura che l'immagine venga caricata prima di disegnarla sul Canvas */
+    c.getContext('2d').drawImage(myImg, 0,0); /* disegna l'immagine sul Canvas, questa è l'immagine che fa da sfondo al Canvas */
 }
 
+/**
+ * Questa funzione disegna l'immagine indicata da name alle coordinate indicate sul Canvas
+ * identificato dall'ID piattoComposto.
+ * Questa funzione è chiamata con argomenti diversi da ogni radio button distinto sulla pagina Componi Piatto.
+ * @param {string} name - il path dell'immagine da disegnare
+ * @param {number} xcoordinate - le coordinate sull'asse x alle quali bisogna disegnare l'immagine
+ * @param {number} ycoordinate - le coordinate sull'asse y alle quali bisogna disegnare l'immagine
+ */
 function printImage(name, xcoordinate, ycoordinate) {
-    var c = document.getElementById("piattoComposto");
-    var myImg = new Image(597, 618);
-    myImg.src = "media/" + name;
-    myImg.onload = function() {
-        c.getContext('2d').drawImage(myImg, xcoordinate, ycoordinate);
+    var c = document.getElementById("piattoComposto"); /* prende la reference al Canvas per mezzo dell'ID */
+    var myImg = new Image(597, 618); /* crea un nuovo oggetto immagine della dimensione desiderata */
+    myImg.src = "media/" + name; /* imposta la src dell'immagine */
+    myImg.onload = function() { /* si assicura che l'immagine venga caricata prima di disegnarla sul Canvas */
+        c.getContext('2d').drawImage(myImg, xcoordinate, ycoordinate); /* disegna l'immagine desiderata sul Canvas alle coordinate desiderate*/
     }
 }
