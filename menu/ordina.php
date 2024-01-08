eb2fb16dcfee3a3458a78909109b57233d74c991
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-01-08 21:21:18 +0100

    commenti a ordina

--- menu/ordina.php
+++ menu/ordina.php
@@ -1,94 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <title>Menu</title> <!--titolo della scheda-->
+        <meta charset="utf-8"> <!--character encoding-->
+        <base href="http://localhost/Flying_Sauce_r/"> <!--base degli href della pagina-->
+        <link rel="stylesheet" href="menu/ordina.css"> <!--collegamento al foglio di stile-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> <!--collegamento al font-->
+    </head>
 
-<head>
-    <title>CSS Template</title>
-    <meta charset="utf-8">
-    <base href="http://localhost/Flying_Sauce_r/">
-    <link rel="stylesheet" href="menu/ordina.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
-</head>
+    <body>
+        <?php require "../base/navSimple.php" ?> <!--inserimento navbar-->
+        
+        <div class="row"> <!--righa con tutto il contenuto della pagina-->
+            <div class="columnside sx"> <!--colonna di sinistra della pagina-->
+                <h1>I nostri piatti</h1> <!--titolo di questa sezione della pagina e collegamenti alle varie parti del menu-->
+                <a href="menu/ordina.php/#classici" class="piatti">CLASSICI</a>
+                <a href="menu/ordina.php/#rossi" class="piatti">ROSSI</a>
+                <a href="menu/ordina.php/#bianchi" class="piatti">BIANCHI</a>
+                <a href="menu/ordina.php/#mare" class="piatti">MARE</a>
+                <a href="menu/ordina.php/#vegetariani" class="piatti">VEGETARIANI</a>
+                <a href="menu/ordina.php/#vegani" class="piatti">VEGANI</a>
+                <p class="domanda"> <!--paragrafo che introduce il bottone per andare alla pagina Componi Piatto-->
+                    Non hai trovato ciò che fa per te?
+                </p>
+                <a class="menu_buttons" href="componi%20piatto/cPiattoDef.php">COMPONI ORA IL TUO PIATTO</a> <!--bottone per andare alla pagina Componi Piatto-->
+            </div>
 
-<body>
-    <?php require "../base/navSimple.php"?>
-    <div class="row">
-        <div class="columnside sx">
-            <h1>I nostri piatti</h1>
-
-            <a href="menu/ordina.php/#classici" class="piatti">CLASSICI</a>
-            <a href="menu/ordina.php/#rossi" class="piatti">ROSSI</a>
-            <a href="menu/ordina.php/#bianchi" class="piatti">BIANCHI</a>
-            <a href="menu/ordina.php/#mare" class="piatti">MARE</a>
-            <a href="menu/ordina.php/#vegetariani" class="piatti">VEGETARIANI</a>
-            <a href="menu/ordina.php/#vegani" class="piatti">VEGANI</a>
-
-            <p class="domanda">
-                Non hai trovato ciò che fa per te?
-            </p>
-            <a class="menu_buttons" href="componi%20piatto/cPiattoDef.php">COMPONI ORA IL TUO PIATTO</a>
-
-            <!-- era equivalente, anzi anche più flessibile fare <button type="submit"> se magari volevi risaltare solo una parola nel bottone -->
-        </div>
-        <div class="content">
-            <div class="scrollable_content">
-                <?php
-                    $host="localhost";
-                    $db='GruppoXX';
-                    $user="www";
-                    $password="password";
-                    $connection_string = "host=$host dbname=$db user=$user password=$password";
-                    $db = pg_connect($connection_string) or die('Impossibile connettersi al database: '.pg_last_error());
-                    
-                    $categories = array("Classici", "Rossi", "Bianchi", "Mare", "Vegetariani", "Vegani");
-                    foreach( $categories as $curr_cat ) {
-                        ?>
-                        <h1 class="titolo" id="<?php echo strtolower($curr_cat); ?>"><?php echo $curr_cat; ?></h1>
-                        <div class="categoria">
-
-                        <?php
-                        $sql = "SELECT nome, descrizione_breve, foto FROM menu WHERE categoria = '$curr_cat'";
-                        $ret = pg_query($db, $sql);
+            <div class="content"> <!--contenuto centrale della pagina-->
+                <div class="scrollable_content"> <!--div che identifica la parte di pagina "scrollabile"-->
+                    <?php
+                        /*connessione al database*/
+                        $host="localhost";
+                        $db='GruppoXX';
+                        $user="www";
+                        $password="password";
+                        $connection_string = "host=$host dbname=$db user=$user password=$password"; /* viene inizializzata una stringa di connessione */
+                        $db = pg_connect($connection_string) or die('Impossibile connettersi al database: '.pg_last_error()); /* inizializza la connessione */
                         
-                        while( $row = pg_fetch_array($ret) ) {    
-                            $nome = $row[0];
-                            $descrizione_breve = $row[1];
-                            $foto = $row[2];
-                        ?>
-                            <div class="primi">
-                                <form action="menu/piatto_singolo/piatto.php" method="post" id="to_piatto_singolo">
-                                    <button type="submit" id="info_button">
-                                        <input type="hidden" name="name" value="<?=$nome?>">
-                                        <img src="media/info_button" alt="info-dish" width="24px" height="24px"/>
-                                    </button>
-                                </form>
-                                <a href="#">
-                                    <img src="<?php echo $foto ?>" alt="piatto di pasta" width="230px">
-                                    <div class="nomepiatto"><?php echo $nome ?></div>
-                                    <?php echo $descrizione_breve ?>
-                                </a>
+                        $categories = array("Classici", "Rossi", "Bianchi", "Mare", "Vegetariani", "Vegani"); /* creazione di un array che contiene i nomi delle categorie del menu */
+                        foreach( $categories as $curr_cat ) { /* un ciclo che inserisce i piatti per ogni categoria */
+                            ?> <!--chiusura del blocco php perché bisogna eseguire ciclicamente codice che è misto html/php-->
+                            <h1 class="titolo" id="<?php echo strtolower($curr_cat); ?>"><?php echo $curr_cat; ?></h1> <!--titolo della categoria-->
+                            <div class="categoria"> <!--div che contiene tutti gli elementi della categoria corrente-->
+                                <?php
+                                    $sql = "SELECT nome, descrizione_breve, foto FROM menu WHERE categoria = '$curr_cat'"; /* interrogazione SQL
+                                    sulla tabella menu delle informazioni specificate dopo SELECT del piatto che ha il nome ricevuto dal form */
+                                    $ret = pg_query($db, $sql); /* viene eseguita la query */
+                                    
+                                    while( $row = pg_fetch_array($ret) ) { /* while che gira ogni piatto della categoria corrente */
+                                        $nome = $row[0];
+                                        $descrizione_breve = $row[1];
+                                        $foto = $row[2];
+                                ?>
+                                        <div class="primi"> <!--div che contiene il singolo piatto-->
+                                            <form action="menu/piatto_singolo/piatto.php" method="post" id="to_piatto_singolo"> <!--form per accedere alla pagina del singolo piatto selezionato-->
+                                                <button type="submit" id="info_button"> <!--bottone di submit che è un'immagine-->
+                                                    <input type="hidden" name="name" value="<?=$nome?>"> <!--per spedire l'informazione del nome del piatto c'è un input type hidden-->
+                                                    <img src="media/info_button" alt="info-dish" width="24px" height="24px"/> <!--immagine "info" per eseguire la submit-->
+                                                </button>
+                                            </form>
+                                            <a href="#"> <!--anchor perché cliccando si deve aggiungere al carrello (WORK IN PROGRESS, ancora non implementato)-->
+                                                <img src="<?php echo $foto ?>" alt="piatto di pasta" width="230px"> <!--immagine del piatto-->
+                                                <div class="nomepiatto"><?php echo $nome ?></div> <!--nome del piatto-->
+                                                <?php echo $descrizione_breve ?> <!--descrizione breve del piatto-->
+                                            </a>
+                                        </div>
+                                <?php        
+                                    } /* chiusura del while */
+                                ?>
                             </div>
-                        <?php        
-                        }
-                        ?>
-                        </div>
-                        <?php
-                    }
-                ?>
+                    <?php
+                        } /* chiusura del foreach */
+                    ?>
+                </div>
+            </div>
+            <div class="columnside dx"> <!--colonna destra WORK IN PROGRESS-->
+                <form action="componi">
+                    <input type="submit" value="VAI AL PAGAMENTO" name="pagamento" class="menu_buttons" />
+                </form>
+                <ul>
+                    <li>Spaghetti alla carbonara x2</li>
+                    <li>pasta alla norma x3</li>
+                </ul>
             </div>
         </div>
-        <div class="columnside dx">
-            <form action="componi">
-                <input type="submit" value="VAI AL PAGAMENTO" name="pagamento" class="menu_buttons" />
-            </form>
-            <ul>
-                <li>Spaghetti alla carbonara x2</li>
-                <li>pasta alla norma x3</li>
-            </ul>
-        </div>
-    </div>
-    <?php
-        require "../base/footer.php";
-    ?>
-</body>
+        <?php require "../base/footer.php"; ?> <!--inserimento footer-->
+    </body>
 
 </html>
