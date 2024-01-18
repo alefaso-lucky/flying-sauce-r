ed96feecff7074b9bc3dfccfa621e3e6f1ca3a8c
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-01-18 18:48:16 +0100

    aggiunta logica per aumentare la quantita di ordini nel carrello

--- menu/updateCart.php
+++ menu/updateCart.php
@@ -8,12 +8,24 @@
     $db = pg_connect($connection_string) or die('Impossibile connettersi al database: '.pg_last_error()); /* inizializza la connessione */
 
     if(isset($_POST['name_piatto'])) {
-        echo "ciao";
         $name_piatto = $_POST['name_piatto'];
-        $sql = "INSERT INTO carrello (piatto, email) VALUES ('$name_piatto', 'test')";
-        $ret = pg_query($db, $sql); /* viene eseguita la query */
-        if($ret) {
-            echo "successo";
+
+        //logica per aumentare il numero
+        $check = "SELECT quantita FROM carrello WHERE piatto = '$name_piatto' AND email = 'test'"; //manca il modo per far arrivare l'email qui
+        $ret_select = pg_query($db, $check);
+        if(pg_num_rows($ret_select) > 0) {
+            //update logic
+            $row = pg_fetch_array($ret_select);
+            $quantita = $row['quantita'] + 1;
+            $updateQuery = "UPDATE carrello SET quantita = '$quantita' WHERE piatto = '$name_piatto' AND email = 'test'";
+            pg_query($db, $updateQuery);
+        }
+        else {
+            $sql = "INSERT INTO carrello (piatto, email, quantita) VALUES ('$name_piatto', 'test', 1)";
+            $ret_insert = pg_query($db, $sql); /* viene eseguita la query */
+            if($ret_insert) {
+                echo "successo";
+            }
         }
     }
     
