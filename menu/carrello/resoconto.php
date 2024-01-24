1524f6778e9b6d9e58ac4e7c4eb39c3ed5a376c7
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-01-24 16:33:19 +0100

    php in carrello

--- menu/carrello/resoconto.php
+++ menu/carrello/resoconto.php
@@ -3,13 +3,50 @@
     <meta charset="utf-8">
 	<base href="http://localhost/Flying_Sauce_r/">
 	<link rel="stylesheet" href="./registrati/resoconto.css">
+    <?php
+        session_start();
+        if(isset($_SESSION['loggato']) && $_SESSION['loggato']) {
+            $logged = $_SESSION['loggato'];
+            $email_user = $_SESSION['email'];
+        }
+        else {
+            header("Location: http://localhost/Flying_Sauce_r/accedi/accediSimple.php"); 
+            exit();
+        }
+
+    ?>
 </head>
 <body>
     <div id="row">
         <h1>CARRELLO</h1>
         <table>
             <tr><th>Pietanza</th><th>Quantità</th><th>Prezzo</th></tr>
-            
+            <?php
+                /*connessione al database*/
+                $host="localhost";
+                $db='GruppoXX';
+                $user="www";
+                $password="password";
+                $connection_string = "host=$host dbname=$db user=$user password=$password"; /* viene inizializzata una stringa di connessione */
+                $db = pg_connect($connection_string) or die('Impossibile connettersi al database: '.pg_last_error()); /* inizializza la connessione */
+
+                $cart = "SELECT piatto, quantita FROM carrello WHERE email = '$email_user'";
+                $cart_query = pg_query($db, $cart);
+                while($row = pg_fetch_array($cart_query)) {
+                    $piatto = $row[0];
+                    $quantita = $row[1];
+                    $price_menu = "SELECT prezzo FROM menu WHERE nome = '$piatto'";
+                    $price_menu_query = pg_query($db, $price_menu);
+                    $row_price = pg_fetch_array($price_menu_query);
+                    if(!$row_price) {
+                        echo "<tr><td>".$piatto."</td><td>".$quantita."</td><td>"."data not available"."</td></tr>";
+                    }
+                    else {
+                        $price = $row_price[0] * $quantita;
+                        echo "<tr><td>".$piatto."</td><td>".$quantita."</td><td>".$price."</td></tr>";
+                    }
+                }
+            ?>
         </table>
         <a id="bottone_secondario" href="#">INDIETRO</a> <!--bottone per andare alla pagina precedente-->
         <a id="bottone_primario" href="#">AVANTI</a> <!--bottone per andare alla pagina successiva-->
