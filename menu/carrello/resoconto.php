8efcdd163babd0660435016886e6581fcec625d6
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-01-24 17:44:46 +0100

    indentazione

--- menu/carrello/resoconto.php
+++ menu/carrello/resoconto.php
@@ -4,7 +4,7 @@
 	<base href="http://localhost/Flying_Sauce_r/">
 	<link rel="stylesheet" href="./menu/carrello/resoconto.css">
     <?php
-    /*
+    
         session_start();
         if(isset($_SESSION['loggato']) && $_SESSION['loggato']) {
             $logged = $_SESSION['loggato'];
@@ -14,7 +14,7 @@
             header("Location: http://localhost/Flying_Sauce_r/accedi/accediSimple.php"); 
             exit();
         }
-    */
+    
     ?>
 </head>
 <body>
@@ -47,6 +47,7 @@
                         echo "<tr><td>".$piatto."</td><td>".$quantita."</td><td>".$price."</td></tr>";
                     }
                 }
+                pg_close($db);
             ?>
         </table>
         <div>
