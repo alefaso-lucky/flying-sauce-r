824b46e477254864a61ae945bac37bbff3104514
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2024-01-24 16:32:23 +0100

    inizio stile carrello

--- menu/carrello/resoconto.php
+++ menu/carrello/resoconto.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8">
 	<base href="http://localhost/Flying_Sauce_r/">
-	<link rel="stylesheet" href="./registrati/resoconto.css">
+	<link rel="stylesheet" href="./menu/carrello/resoconto.css">
 </head>
 <body>
     <div id="row">
@@ -11,8 +11,10 @@
             <tr><th>Pietanza</th><th>Quantità</th><th>Prezzo</th></tr>
             
         </table>
-        <a id="bottone_secondario" href="#">INDIETRO</a> <!--bottone per andare alla pagina precedente-->
-        <a id="bottone_primario" href="#">AVANTI</a> <!--bottone per andare alla pagina successiva-->
+        <div>
+            <a id="bottone_secondario" href="#">INDIETRO</a> <!--bottone per andare alla pagina precedente-->
+            <a id="bottone_primario" href="#">AVANTI</a> <!--bottone per andare alla pagina successiva-->
+        </div>
     </div>
 </body>
 </html>
