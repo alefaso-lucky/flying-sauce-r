7dbe766afefc6b86fc31ed8261cc79328e0c1ba7
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2024-01-24 15:49:40 +0100

    inizio carrello

---
+++ menu/carrello/resoconto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+	<base href="http://localhost/Flying_Sauce_r/">
+	<link rel="stylesheet" href="./registrati/resoconto.css">
+</head>
+<body>
+    <div id="row">
+        <h1>CARRELLO</h1>
+        <table>
+            <tr><th>Pietanza</th><th>Quantità</th><th>Prezzo</th></tr>
+            
+        </table>
+        <a id="bottone_secondario" href="#/ordina.php">COMPONI ORA IL TUO PIATTO</a> <!--bottone per andare alla pagina Componi Piatto-->
+    </div>
+</body>
+</html>
