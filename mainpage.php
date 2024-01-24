fc5adf3ddb7a135f84b85ea360a81dc0e496668f
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2024-01-24 20:21:53 +0100

    creazione secondo blocco della home

--- mainpage.php
+++ mainpage.php
@@ -3,12 +3,22 @@
     <head>
         <title>Flying Sauce&reg;</title>
         <meta charset="utf-8"/>
+        <link rel="stylesheet" href="mainpage.css">
     </head>
     <body>
-        <?php require 'navbar.php'; ?>
-
-        <?php
-            include "footer.php";
-        ?>
+        <div id=contenitore_2>
+            <div id=sx>
+                <p id=frasesx>suggerimenti dello chef</p>
+                <a class="bot" href="#">STUPISCITI</a> <!--bottone per andare alla pagina Stupisciti-->
+            </div>
+            <div id=cc>
+                <p id=frasecc>dall'idea alla forchetta</p>
+                <a class="bot" href="#">COMPONI ORA</a> <!--bottone per andare alla pagina Componi Piatto-->
+            </div>
+            <div id=dx>
+            <p id=frasedx>l'insuperabile proposta</br>italiana</p>
+                <a class="bot" href="#">MENU</a> <!--bottone per andare alla pagina Menu-->
+            </div>
+        </div>
     </body>
 </html>
