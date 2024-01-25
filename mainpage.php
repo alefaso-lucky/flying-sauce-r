352e19c35a3a810992bf7b1b299a84483052d99d
alefaso-lucky <150935261+alefaso-lucky@users.noreply.github.com>
2024-01-25 10:10:47 +0100

    creazione terzo blocco della home

--- mainpage.php
+++ mainpage.php
@@ -6,19 +6,34 @@
         <link rel="stylesheet" href="mainpage.css">
     </head>
     <body>
+        <!--secondo blocco, tre sezioni che conducono a tre percorsi diversi del sito-->
         <div id=contenitore_2>
-            <div id=sx>
+            <div id=sx2>
                 <p id=frasesx>suggerimenti dello chef</p>
                 <a class="bot" href="#">STUPISCITI</a> <!--bottone per andare alla pagina Stupisciti-->
             </div>
-            <div id=cc>
+            <div id=cc2>
                 <p id=frasecc>dall'idea alla forchetta</p>
                 <a class="bot" href="#">COMPONI ORA</a> <!--bottone per andare alla pagina Componi Piatto-->
             </div>
-            <div id=dx>
+            <div id=dx2>
                 <p id=frasedx>la proposta italiana</p>
                 <a class="bot" href="#">MENU</a> <!--bottone per andare alla pagina Menu-->
             </div>
+        </div>
+        <!--terzo blocco, pasta della settimana-->
+        <div id=contenitore_3>
+            <div id=sx3>
+                <h1>LA PASTA DELLA SETTIMANA:</h1>
+                <h2>La Lasagna</h2>
+                <p>
+                    Pasta fresca, un impasto realizzato con cura, con uova biologiche e farina di 
+                    grano tenero con carne di manzo Wagyu, delicatamente marinata con erbe aromatiche 
+                    e vino rosso. Questa lasagna, ispirata alla nonna, è molto più di un semplice 
+                    pasto. È un viaggio sensoriale, un tributo all’amore e alla tradizione.
+                </p>
+            </div>
+            <img src="media/home/pastaDellaSettimana.jpg" alt="la pasta della settimana è lasagna alla bolognese">
         </div>
     </body>
 </html>
