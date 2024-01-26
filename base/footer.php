2f437afe48bec2e3fa042a1ba48c9135433a89f4
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-01-26 13:09:47 +0100

    newsletter funzionante

--- base/footer.php
+++ base/footer.php
@@ -1,13 +1,44 @@
 <link rel="stylesheet" href="base/footer.css" type="text/css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <footer>
+    <?php
+        if(isset($_POST['email_newsletter'])) {
+            $email_newsletter = $_POST['email_newsletter'];
+            /*connessione al database*/
+            $host="localhost";
+            $db='GruppoXX';
+            $user="www";
+            $password="password";
+            $connection_string = "host=$host dbname=$db user=$user password=$password"; /* viene inizializzata una stringa di connessione */
+            $db = pg_connect($connection_string) or die('Impossibile connettersi al database: '.pg_last_error()); /* inizializza la connessione */
+
+            //controllo per vedere se l'utente è già iscritto
+            $newsletter_search = "SELECT email FROM newsletter WHERE email = '$email_newsletter'";
+            $newsletter_search_query = pg_query($db, $newsletter_search);
+            if(pg_num_rows($newsletter_search_query) > 0) {
+                echo "<script>alert("."'Sei già iscritto, grazie di far parte di questa grande famiglia!'".");</script>";
+            }
+            else {
+                $newsletter_add = "INSERT INTO newsletter (email) VALUES ($1)";
+                $newsletter_add_prepare = pg_prepare($db, "newsletter_add", $newsletter_add);
+                $newsletter_add_query = pg_execute($db, "newsletter_add", array($email_newsletter));
+                if(!$newsletter_add_query) {
+                    echo "<script>alert("."'Impossibile completare iscrizione'".");</script>";
+                }
+                else {
+                    echo "<script>alert("."'Iscrizione completata con successo'".");</script>";
+                }
+            }
+            pg_close($db);
+        }
+    ?>
     <div id="footer_container">
         <div id="upper_footer_container">
             <div id="newsletter_text">
                 <h1>Iscriviti alla nostra newsletter!</h1>
                 <p id="margin_less">Riceverai sconti, regali e inviti ad eventi!</p>
             </div>
-            <form action="#UNKNOWN" method="post" id="newsletter_form">
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="newsletter_form">
                 <label for="newsletter_field">e-mail: <input type="email" id="newsletter_field" name="email_newsletter" placeholder="Inserisci qui la tua e-mail" required></label>
                 <input type="submit" value="REGISTRAMI" id="submit_newsletter_footer">
             </form>
