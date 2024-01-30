138b151179ba2b11098920c85e179567e963ca72
BeneCelano <benedettacelano@gmail.com>
2024-01-30 18:50:45 +0100

    nav e footer a chi siamo, rimaneggiamenti profilo

--- chi siamo/chi siamo.php
+++ chi siamo/chi siamo.php
@@ -2,14 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="chi siamo.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <title>Chi siamo</title>
-    <script src="./chi siamo.js"></script>
+    <meta name="author" content="Gruppo08">
+    <meta name="description" content="Una pagina statica che descrive i valori del sito">
+    <meta name="keywords" content="pasta droni italia cucina FlyingSauce spaghetti">
+    <link rel="icon" href="./media/favicon.ico" type="image/x-icon">
+	  <base href="http://localhost/Flying_Sauce_r/">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="chi siamo/chi siamo.css">
+    <script src="chi siamo/chi siamo.js"></script>
 </head>
 <body>
-    <?php //require '../base/nav.php' ?>
+    <?php require "../base/navFINITA.php" ; ?>
 
     <!-- div per contenere uno slider per le slides -->
     <div class="background-container">
@@ -22,7 +26,7 @@
           <input type="radio" class="radio" name="images" id="radio-4" onclick="showSlide(4)">
 
           <div class="slide" id="slide-1">
-            <img src="../media/chi_siamo1.jpg" alt="">
+            <img src="media/chi_siamo1.jpg" alt="">
             <div class="slide-text"><strong>Le nostre origini</strong></br>
               Benvenuti nel cuore della nostra tradizione culinaria, dove la storia della nostra pasta si intreccia con secoli di passione e maestria. La nostra avventura inizia nelle cucine delle famiglie più autentiche, dove ricette tramandate di generazione in generazione sono diventate la base della nostra filosofia gastronomica.
               La nostra pasta racconta storie di antiche tradizioni e segreti tramandati, mescolati con la freschezza di ingredienti accuratamente selezionati. Ogni forma e taglio riflettono la nostra dedizione per offrire un'esperienza culinaria autentica e memorabile.
@@ -31,7 +35,7 @@
             </div>
           </div>
           <div class="slide" id="slide-2">
-            <img src="../media/chi_siamo2.jpg" alt="">
+            <img src="media/chi_siamo2.jpg" alt="">
             <div class="slide-text"><strong>Chef - le nostre Stelle Michelin</strong></br>
               Nel 2020, l'annuncio delle tre stelle Michelin per il mio piatto "Rosso Rubino e Verde Sinfonia" è stato un momento di straordinaria gioia e tensione. Questo piatto, frutto di dedizione e ispirazione nella natura, combina audacemente mandorle tostate e barbabietole lavorate con varie tecniche per creare un equilibrio unico di sapori. La presentazione è concepita come un'opera d'arte culinaria, coinvolgendo tutti i sensi con colori vibranti.
               Il prestigioso riconoscimento è un apice nella mia carriera, sottolineando l'impegno per l'eccellenza gastronomica e il contributo fondamentale del mio team. Mantenere gli standard elevati è ora una responsabilità più profonda, e affronto con serietà l'impegno di soddisfare le aspettative del pubblico.
@@ -39,14 +43,14 @@
             </div>
           </div>
           <div class="slide" id="slide-3">
-            <img src="../media/chi_siamo3.png" alt="">
+            <img src="media/chi_siamo3.png" alt="">
             <div class="slide-text"><strong>Ingegneri - non solo pasta </strong></br>
               Come capo ingegnere di "Flying Sauce", ho guidato lo sviluppo di una flotta avanzata di droni per la consegna globale di pasta calda. La sfida principale era mantenere la temperatura ottimale durante il trasporto, risolta con successo attraverso un isolamento termico avanzato e un algoritmo intelligente. Abbiamo progettato hardware specifico per affrontare distanze considerevoli, ottimizzando l'efficienza delle operazioni di volo e garantendo autonomia sufficiente.
               Dal punto di vista della comunicazione, abbiamo implementato tecnologie avanzate per mantenere una comunicazione costante tra i droni e il centro di controllo. La sicurezza è stata una priorità con l'integrazione di sensori avanzati e algoritmi intelligenti. A livello software, abbiamo sviluppato algoritmi di intelligenza artificiale per ottimizzare le consegne.
             </div>
           </div>
           <div class="slide" id="slide-4">
-            <img src="../media/chi_siamo4.jpg" alt="">
+            <img src="media/chi_siamo4.jpg" alt="">
             <div class="slide-text"><strong>Il futuro è oggi</strong></br>
               Siamo entusiasti di estendere la nostra passione per la cucina italiana a livello globale attraverso il nostro innovativo servizio di consegna. Grazie ai nostri droni all'avanguardia, possiamo garantire che piatti italiani di altissima qualità raggiungano qualsiasi parte del mondo, rappresentando una svolta nel settore della consegna. Questo ci consente di offrire un'esperienza autentica a chiunque, ovunque si trovi.
               La tecnologia avanzata di tracciabilità consente ai clienti di seguire in tempo reale il percorso dei loro piatti, garantendo efficienza e trasparenza nel processo di consegna. La flessibilità dei supedroni ci permette di spostare i nostri chef in diverse regioni italiane, offrendo itinerari di gusto dinamici che esplorano la ricchezza culinaria del nostro paese.
@@ -84,7 +88,7 @@
       <!-- utilizzo JS per cambiare l'immagine in una versione più grigia -->
       <div class="grid-img-container">
           <div class="grid-item" id="grid-item1">
-            <img src="../media/gridImg1.jpg" id="img1"></img>
+            <img src="media/gridImg1.jpg" id="img1"></img>
             <div class="overlay">
               <p class="testoImg" id="testoImg1">
                 Il coraggio di intraprendere nuove strade
@@ -92,21 +96,21 @@
             </div>
           </div>
           <div class="grid-item" id="grid-item2">
-            <img src="../media/gridImg2.jpg" id="img2"></img>
+            <img src="media/gridImg2.jpg" id="img2"></img>
             <div class="overlay">
               <p class="testoImg" id="testoImg2">
                 L'impegno nel perseguire i propri sogni
             </div>
           </div>
           <div class="grid-item" id="grid-item3">
-            <img src="../media/gridImg3.jpg" id="img3"></img>
+            <img src="media/gridImg3.jpg" id="img3"></img>
             <div class="overlay">
               <p class="testoImg" id="testoImg3">
                 La possibilità di esportare i gusti italiani in tutto il mondo
             </div>
           </div>
           <div class="grid-item" id="grid-item4">
-            <img src="../media/gridImg4.jpg" id="img4"></img>
+            <img src="media/gridImg4.jpg" id="img4"></img>
             <div class="overlay">
               <p class="testoImg" id="testoImg4">
                 L'autenticità del nostro brand
@@ -114,5 +118,6 @@
             </div>
           </div>
       </div>
+      <?php require "../base/footer.php"; ?> <!--inserimento footer-->
 </body>
 </html>
--- membership/area_riservata/profilo.js
+++ membership/area_riservata/profilo.js
@@ -1,48 +1,77 @@
-function switchDiv(visibleDiv){
-    var specialDiv = document.getElementById(visibleDiv);
-    var allSections = document.getElementsByClassName("sezione");
+document.addEventListener("DOMContentLoaded", function () {
+    var selectedDiv = sessionStorage.getItem("selected"); // questa variabile di sessione è l'ID del div che cliccato seleziona il div visibile;
+    document.getElementById("sel" + selectedDiv).click(); // clicca il div di selezione del div visibile
+    if(selectedDiv == "Spedizione") // se il div da visializzare è "Spedizione" allora è necessario capire quale form visualizzare
+      visualizzaFormSpedizione();
+  });
   
-    for(var i=0; i< allSections.length; i++){
-      allSections[i].style.display= "none";     /*rendo invisibili prima tutti i div che appartengono alla classe sezione*/
-    }
-    specialDiv.style.display= "block";    /*procedo poi a rendere nuovamente visibile il div che è stato selezionato*/
+  /* questa funzione viene chiamata all'onclick dei div di selezione. Cambia la visibilià dei div di informazioni rendnendo visibile il div selezionato dall'utente */
+  function switchDiv(visibleDiv){
+      var specialDiv = document.getElementById(visibleDiv);
+      var allSections = document.getElementsByClassName("sezione");
   
-    coloreSelezionato(visibleDiv);      /*per rendere più particolare il colore della sezione in cui ti trovi*/
-}
+      for(var i=0; i< allSections.length; i++){
+        allSections[i].style.display= "none";     /*rendo invisibili prima tutti i div che appartengono alla classe sezione*/
+      }
   
-function coloreSelezionato(visibleDiv){
-    var otherSel = document.getElementsByClassName('selezione');
-
-    for(var i=0; i<otherSel.length; i++){
-        otherSel[i].style.background= "#46666D";
-
-        if(otherSel[i].id == "sel"+visibleDiv)
-            otherSel[i].style.background="rgb(255, 174, 88)";
-    }
-}
+      sessionStorage.setItem("selected", visibleDiv); /* modifica la variabile di sessione che memorizza quale div è selezionato */
   
-function visibleForm(idVisible, idInvisible){
-    document.getElementById(idInvisible).style.display = 'none';
-    document.getElementById(idVisible).style.display = 'block';
-}
+      specialDiv.style.display= "block";    /*procedo poi a rendere nuovamente visibile il div che è stato selezionato*/
   
-function validaModulo_pass(form){
-//non effettuo controlli se i campi sono pieni poichè ho esplicitato con html la parola required
-    if (form.newpass.value != form.repassword.value) {					//controllo se password e conferma coincidono
-        alert("Le due password non corrispondono");
-        form.newpass.focus();
-        form.newpass.select();
-        return false;
-    }else{			//se coincidono procedo a controllare se la password rispetta i canoni
-        password = form.newpass.value;
-        // Almeno 8 caratteri, una lettera maiuscola, un numero e un simbolo speciale
-        const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()\-_=\+{};:,<\.>]).{8,}$/;
-        if(passwordRegex.test(password) == false){
-        alert("La password deve contenere almeno 8 caratteri, una lettera maiuscola, un numero e un carattere speciale.");
-        return false;
-        }
-        return true;
-    }
-}
-
+      coloreSelezionato(visibleDiv);      /*per rendere più particolare il colore della sezione in cui ti trovi*/
+  }
   
+  /* questa funzione serve per cambiare il colore di backgroud dei div di selezione */
+  function coloreSelezionato(visibleDiv){
+      var otherSel = document.getElementsByClassName('selezione');
+  
+      for(var i=0; i<otherSel.length; i++){
+          otherSel[i].style.background= "#46666D";
+  
+          if(otherSel[i].id == "sel"+visibleDiv)
+              otherSel[i].style.background="rgb(255, 174, 88)";
+      }
+  }
+  
+  /* questa funzione viene chiamata quando l'utente seleziona quale form di "Spedizione" visualizzaare */
+  function visibleForm(idVisible, idInvisible){
+      document.getElementById(idInvisible).style.display = 'none';
+      document.getElementById(idVisible).style.display = 'block';
+      sessionStorage.setItem("visibleSpedizione", idVisible); /* memorizzo nella variabile di sessione quale form di "Spedizione" è selezionato (info o modifica) */
+  }
+  
+  /* funzione di utilità per visualizzare il form di "Spedizione" */
+  function visualizzaFormSpedizione() {
+      console.log("SONO IN VISUALIZZAFORMSPEDIZIONE");
+      // se è selezionato il div di "Spedizione" allora è necessario specificare se far apparire le info di spedizione o il form per modificare l'indirizzo.
+      switch(sessionStorage.getItem("visibleSpedizione")) { // valore della variabile di sessione che memorizza quale form visualizzare
+        case 'info-indirizzo': /* caso in cui il form visibile è quello delle info */
+          document.getElementById("changeToInfo").click();
+          break;
+        case 'aggiorna-indirizzo': /* caso in cui il fomr visibile è quello per la modifica delle informazioni */
+          document.getElementById("changeToAggiorna").click();
+          break;
+        default: /* di default mostra le informazioni */
+          document.getElementById("changeToInfo").click();
+          break;
+      }
+  }
+  
+  function validaModulo_pass(form){
+  //non effettuo controlli se i campi sono pieni poichè ho esplicitato con html la parola required
+      if (form.newpass.value != form.repassword.value) {					//controllo se password e conferma coincidono
+          alert("Le due password non corrispondono");
+          form.newpass.focus();
+          form.newpass.select();
+          return false;
+      }else{			//se coincidono procedo a controllare se la password rispetta i canoni
+          password = form.newpass.value;
+          // Almeno 8 caratteri, una lettera maiuscola, un numero e un simbolo speciale
+          const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()\-_=\+{};:,<\.>]).{8,}$/;
+          if(passwordRegex.test(password) == false){
+          alert("La password deve contenere almeno 8 caratteri, una lettera maiuscola, un numero e un carattere speciale.");
+          return false;
+          }
+          return true;
+      }
+  }
