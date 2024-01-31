9bfc32ea97166bf1341e68381ba3e2e6c75e9060
BeneCelano <benedettacelano@gmail.com>
2024-01-31 17:34:35 +0100

    accorgimenti chi siamo

--- chi siamo/chi siamo.php
+++ chi siamo/chi siamo.php
@@ -18,7 +18,6 @@
     <!-- div per contenere uno slider per le slides -->
     <div class="background-container">
       <div class="container">
-
         <div class="slider">
           <input type="radio" class="radio" name="images" id="radio-1" onclick="showSlide(1)">
           <input type="radio" class="radio" name="images" id="radio-2" onclick="showSlide(2)">
@@ -26,33 +25,34 @@
           <input type="radio" class="radio" name="images" id="radio-4" onclick="showSlide(4)">
 
           <div class="slide" id="slide-1">
-            <img src="media/chi_siamo1.jpg" alt="">
-            <div class="slide-text"><strong>Le nostre origini</strong></br>
+            <img src="media/chi_siamo1.jpg" alt="chi_siamoIMG1" width="800px" height="500px">
+            <div class="slide-text"><h1>Le nostre origini</h1>
               Benvenuti nel cuore della nostra tradizione culinaria, dove la storia della nostra pasta si intreccia con secoli di passione e maestria.
               La nostra avventura inizia nelle cucine delle famiglie più autentiche, dove ricette tramandate di generazione in generazione sono diventate la base della nostra filosofia gastronomica.
               Attraverso il nostro percorso, abbiamo celebrato la diversità regionale e l'amore per l'eccellenza culinaria.
-              Unisciti a noi in questo viaggio gastronomico, portando il calore delle tradizioni direttamente sulla tua tavola.
             </div>
           </div>
           <div class="slide" id="slide-2">
-            <img src="media/chi_siamo2.jpg" alt="">
-            <div class="slide-text"><strong>Chef - le nostre Stelle Michelin</strong></br>
-              Nel 2020, l'annuncio delle tre stelle Michelin per il mio piatto "Rosso Rubino e Verde Sinfonia" è stato un momento di straordinaria gioia e tensione. Questo piatto combina audacemente mandorle tostate e barbabietole lavorate con varie tecniche per creare un equilibrio unico di sapori. La presentazione è concepita come un'opera d'arte culinaria, coinvolgendo tutti i sensi con colori vibranti.
-              Il prestigioso riconoscimento è un apice nella mia carriera, sottolineando l'impegno per l'eccellenza gastronomica e il contributo fondamentale del mio team. Mantenere gli standard elevati è ora una responsabilità più profonda, e affronto con serietà l'impegno di soddisfare le aspettative del pubblico.
+            <img src="media/chi_siamo2.jpg" alt="chi_siamoIMG2" width="800px" height="500px">
+            <div class="slide-text"><h1>Chef - le nostre Stelle Michelin</h1>
+              L'annuncio delle tre stelle Michelin per il mio piatto "Rosso Rubino e Verde Sinfonia" è stato un momento di straordinaria gioia e tensione. 
+              Questo piatto combina audacemente mandorle tostate e barbabietole lavorate con varie tecniche per creare un equilibrio unico di sapori.
+              Il prestigioso riconoscimento sottolinea l'impegno per l'eccellenza gastronomica: mantenere gli standard elevati per soddisfare le aspettative del pubblico è ora una responsabilità più profonda.
             </div>
           </div>
           <div class="slide" id="slide-3">
-            <img src="media/chi_siamo3.png" alt="">
-            <div class="slide-text"><strong>Ingegneri - non solo pasta </strong></br>
+            <img src="media/chi_siamo3.png" alt="chi_siamoIMG3" width="800px" height="500px">
+            <div class="slide-text"><h1>Ingegneri - non solo pasta </h1>
               Come capo ingegnere di "Flying Sauce", ho guidato lo sviluppo di una flotta avanzata di droni per la consegna globale di pasta calda.
-              La sfida principale era mantenere la temperatura ottimale durante il trasporto, risolta con successo attraverso un isolamento termico avanzato e un algoritmo intelligente. Abbiamo progettato hardware specifico per affrontare distanze considerevoli, ottimizzando l'efficienza delle operazioni di volo e garantendo autonomia sufficiente.
-              Dal punto di vista della comunicazione, abbiamo implementato tecnologie avanzate per mantenere una comunicazione costante tra i droni e il centro di controllo. La sicurezza è stata una priorità con l'integrazione di sensori avanzati e algoritmi intelligenti.
+              La sfida principale era mantenere la temperatura ottimale durante il trasporto, risolta attraverso un isolamento termico avanzato e un algoritmo intelligente.
+              Abbiamo progettato un hardware specifico per affrontare lunghe distanze garantendo autonomia sufficiente e
+              tecnologie avanzate per mantenere una comunicazione costante tra i droni e il centro di controllo.
             </div>
           </div>
           <div class="slide" id="slide-4">
-            <img src="media/chi_siamo4.jpg" alt="">
-            <div class="slide-text"><strong>Il futuro è oggi</strong></br>
-              Siamo entusiasti di estendere la nostra passione per la cucina italiana a livello globale attraverso il nostro innovativo servizio di consegna. Grazie ai nostri droni, possiamo garantire che piatti italiani di altissima qualità raggiungano qualsiasi parte del mondo, rappresentando una svolta nel settore della consegna. Questo ci consente di offrire un'esperienza autentica a chiunque, ovunque si trovi.
+            <img src="media/chi_siamo4.jpg" alt="chi_siamoIMG4" width="800px" height="500px">
+            <div class="slide-text"><h1>Il futuro è oggi</h1>
+              Siamo entusiasti di estendere la nostra passione per la cucina italiana a livello globale attraverso il nostro innovativo servizio di consegna. 
               La flessibilità dei supedroni ci permette di spostare i nostri chef in diverse regioni italiane, offrendo itinerari di gusto dinamici che esplorano la ricchezza culinaria del nostro Paese.
               Ogni consegna non rappresenta solo un servizio, ma una missione volta a diffondere il patrimonio culinario italiano in ogni angolo del mondo, un morso alla volta.
             </div>
@@ -65,7 +65,6 @@
           <label for="radio-3" id="label-3"></label>
           <label for="radio-4" id="label-4"></label>
         </div>
-
       </div>
     </div>
 
